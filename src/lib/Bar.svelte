708fe9901180ca653dea5b6918fd7c51f1f6e31f
newkincode <maainnewkin59@gmail.com>
2022-07-21 20:43:03 +0900

    커밋할 내용

---
+++ src/lib/Bar.svelte
@@ -0,0 +1,18 @@
+<div class="main">
+    <b>oc community</b>
+    <a href=""></a>
+</div>
+<style>
+    .main{
+        width: 100%;
+        height: 50px;
+        background-color: rgb(90, 90, 90);
+        position: absolute;
+        left: 0%;
+    }
+    b{
+        position: absolute;
+        top: 10px;
+        left: 15px;
+    }
+</style>
